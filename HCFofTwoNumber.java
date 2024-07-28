99db8f5c0aba8a2bef3f3a79f33f3c702a60af3d
AmourAnurag <anurag875636@gmail.com>
2024-07-28 13:01:51 +0530

    HCF of Two number

---
+++ HCFofTwoNumber.java
@@ -0,0 +1,27 @@
+package AccentureCoding;
+
+import java.util.Scanner;
+
+public class HCFofTwoNumber {
+     public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the first number ");
+        int num1=sc.nextInt();
+        System.out.println("enter the second number ");
+        int num2=sc.nextInt();
+        num1=Math.min(num1, num2);
+        num2=Math.max(num1, num2);
+        int HCF=1;
+        while (true) {
+            int rem=num2%num1;
+            if (rem==0) {
+                HCF=num1;
+                break;
+            }
+            int tmp=num1;
+            num1=rem;
+            num2=tmp;
+        }
+        System.out.println("HCF of both number "+HCF);
+    }
+}
